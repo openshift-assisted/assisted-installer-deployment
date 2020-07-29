b4d85ee4117b5e318028b9b233539ce0e5239951
osher cohen <oscohen@localhost.localdomain>
2020-07-29 20:05:53 +0300

    Move hash update from relase job to repo

--- tools/update_hash.py
+++ tools/update_hash.py
@@ -5,34 +5,39 @@ import yaml
 import sys
 import os
 import re
+import yamlordereddictloader
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--repo", help='repo to update', type=str, required=True)
-parser.add_argument("--hash", help='hash to update', type=str, required=True)
-parser.add_argument("--deployment", help="deployment yaml file to update", type=str,
- default=os.path.join(os.path.dirname(__file__), "../assisted-installer.yaml"))
-args = parser.parse_args()
+
+def update_hash(deployment_yaml, repo, hash):
+        pattern = re.compile(r'\b[0-9a-f]{40}\b')
+        match_SHA1 = re.match(pattern, hash)
+        if not match_SHA1:
+            sys.exit("the passed hash {} is not a valid SHA1 hash".format(hash))
+
+        with open(deployment_yaml, "r") as f:
+            deployment = yaml.load(f, Loader=yamlordereddictloader.Loader)
+        if repo not in deployment:
+            sys.exit("repo {} is not located in the deployment file".format(args.repo))
+        if hash == deployment[repo]:
+            print("repo {} hash is not changed".format(hash))
+            return
+
+
+        deployment[repo]["revision"] = hash
+        with open(deployment_yaml, "w") as f:
+            yaml.dump(deployment, f, default_flow_style=False, Dumper=yamlordereddictloader.Dumper)
+        print("updated {} repo with {} hash".format(repo, hash))
+
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--repo", help='repo to update', type=str, required=True)
+    parser.add_argument("--hash", help='hash to update', type=str, required=True)
+    parser.add_argument("--deployment", help="deployment yaml file to update", type=str,
+     default=os.path.join(os.path.dirname(__file__), "../assisted-installer.yaml"))
+    args = parser.parse_args()
 
-    pattern = re.compile(r'\b[0-9a-f]{40}\b')
-    match_SHA1 = re.match(pattern, args.hash)
-    if not match_SHA1:
-        sys.exit("the passed hash {} is not a valid SHA1 hash".format(args.hash))
-
-    with open(args.deployment, "r") as f:
-        deployment = yaml.safe_load(f)
-    if args.repo not in deployment:
-        sys.exit("repo {} is not located in the deployment file".format(args.repo))
-    if args.hash == deployment[args.repo]:
-        print("repo {} hash is not changed".format(hash))
-        return
-
-
-    deployment[args.repo]["revision"] = args.hash
-    with open(args.deployment, "w") as f:
-        yaml.dump(deployment, f, default_flow_style=False)
-    print("updated {} repo with {} hash".format(args.repo, args.hash))
+    update_hash(deployment=args.deployment, repo=args.repo, hash=args.hash)
 
 if __name__ == "__main__":
     main()
