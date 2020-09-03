70f73f3c52dccb08168523503a897731c9bb3f51
osher cohen <oscohen@localhost.localdomain>
2020-09-03 16:53:13 +0300

    PR fixes

--- tools/assisted_installer_stable_promotion.py
+++ tools/assisted_installer_stable_promotion.py
@@ -12,7 +12,7 @@ IMAGE_FORMAT = "quay.io/ocpmetal/{image_name}:{tag}"
 
 #######################################################################################################
 # This will promote assisted-installer after testing
-# Actions: - tag assisted installer with passed <tag> & <tag>.%d/%m/%Y-%H-%M
+# Actions: - tag assisted-installer-deployment git repo  with passed <tag> & <tag>.%d/%m/%Y-%H-%M
 #          - tag all images described under assisted-installer.yaml with <tag> & <tag>.%d.%m.%Y-%H.%M
 # notice:  - repo has valid push credentials
 #          - working location must be  from in the cloned item
@@ -29,17 +29,14 @@ timestamped_tag = "{tag}.{timestamp}".format(
     tag=args.tag, timestamp=datetime.now().strftime("%d.%m.%Y-%H.%M"))
 
 def main():
-    tag_manifest_image()
+    tag_manifest_images()
     tag_repo(args.tag)
     tag_repo(timestamped_tag)
 
-def tag_manifest_image():
+def tag_manifest_images():
     with open(args.deployment, "r") as f:
         deployment = yaml.load(f)
     for rep, rep_data in deployment.items():
-        project, repo = rep.split("/")
-        if project != "openshift":
-            continue
         for image in rep_data["images"]:
             image_full_name = IMAGE_FORMAT.format(image_name=image, tag=rep_data["revision"])
             try:
@@ -68,7 +65,7 @@ def tag_image(image):
 def tag_repo(tag):
     logging.info("Tagging repo with {tag}".format(tag=tag))
     subprocess.check_output("git tag {tag} -f".format(tag=tag), shell=True)
-    subprocess.check_output("git push -f --tags", shell=True)
+    subprocess.check_output("git push origin {tag} -f".format(tag=tag), shell=True)
 
 
 if __name__ == "__main__":
