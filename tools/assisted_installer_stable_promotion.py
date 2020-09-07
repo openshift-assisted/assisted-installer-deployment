62e45fe5abdb26bc6548693703874e06b7ce2e12
Ronnie Lazar <ronnie.lazar@redhat.com>
2020-09-07 18:24:51 +0300

    added support to use the assisted_installer_stable_promotion tool to tag version-tags
    In version-tags, we do not need to also add {tag}.date

--- tools/assisted_installer_stable_promotion.py
+++ tools/assisted_installer_stable_promotion.py
@@ -23,49 +23,48 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--deployment", help="deployment yaml file to update", type=str,
  default=os.path.join(os.path.dirname(__file__), "../assisted-installer.yaml"))
 parser.add_argument("--tag", help="image tagging", type=str)
+parser.add_argument("--version-tag", help="promote to a version based tag. Will not add tag with date", action='store_true')
 args = parser.parse_args()
 
 timestamped_tag = "{tag}.{timestamp}".format(
     tag=args.tag, timestamp=datetime.now().strftime("%d.%m.%Y-%H.%M"))
 
 def main():
-    tag_manifest_images()
-    tag_repo(args.tag)
-    tag_repo(timestamped_tag)
+    tags = [args.tag]
+    if not args.version_tag:
+        tags.append(timestamped_tag)
 
-def tag_manifest_images():
+    tag_manifest_images(tags)
+    tag_repo(tags)
+
+def tag_manifest_images(tags):
     with open(args.deployment, "r") as f:
         deployment = yaml.load(f)
     for rep, rep_data in deployment.items():
         for image in rep_data["images"]:
             image_full_name = IMAGE_FORMAT.format(image_name=image, tag=rep_data["revision"])
             try:
-                tag_image(image_full_name)
+                tag_image(image_full_name, tags)
             except Exception as ex:
                 logging("Failed to tag {image}, reason: {exception}".format(image=image, exception=ex))
 
-def tag_image(image):
-    logging.info("Tagging image {image} to {tag} & {timestamped_tag}".format(
-        image=image, tag=args.tag, timestamped_tag=timestamped_tag))
+def tag_image(image, tags):
 
     subprocess.check_output("podman pull {}".format(image), shell=True)
 
     # seting images names & tags
-    tagged_image = "{image}:{tag}".format(image=image.rsplit(":")[0], tag=args.tag)
-    timestamp_tagged_image = "{image}:{tag}".format(image=image.rsplit(":")[0], tag=timestamped_tag)
+    for t in tags:
+        logging.info("Tagging image {image} to {tag}".format(
+            image=image, tag=t))
+        tagged_image = "{image}:{tag}".format(image=image.rsplit(":")[0], tag=t)
+        subprocess.check_output("podman tag {} {}".format(image, tagged_image), shell=True)
+        subprocess.check_output("podman push {}".format(tagged_image), shell=True)
 
-    # Tagging images
-    subprocess.check_output("podman tag {} {}".format(image, tagged_image), shell=True)
-    subprocess.check_output("podman tag {} {}".format(image, timestamp_tagged_image), shell=True)
-
-    # Pushing images
-    subprocess.check_output("podman push {}".format(tagged_image), shell=True)
-    subprocess.check_output("podman push {}".format(timestamp_tagged_image), shell=True)
-
-def tag_repo(tag):
-    logging.info("Tagging repo with {tag}".format(tag=tag))
-    subprocess.check_output("git tag {tag} -f".format(tag=tag), shell=True)
-    subprocess.check_output("git push origin {tag} -f".format(tag=tag), shell=True)
+def tag_repo(tags):
+    for t in tags:
+        logging.info("Tagging repo with {tag}".format(tag=t))
+        subprocess.check_output("git tag {tag} -f".format(tag=t), shell=True)
+        subprocess.check_output("git push origin {tag} -f".format(tag=t), shell=True)
 
 
 if __name__ == "__main__":
