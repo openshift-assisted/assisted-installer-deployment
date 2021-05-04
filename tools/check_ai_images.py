37f9601e7b7d5a77c3a09d6878e53c3c08d5ca13
osher cohen <oscohen@localhost.localdomain>
2021-05-04 09:08:17 +0300

    Update daily update

--- tools/check_ai_images.py
+++ tools/check_ai_images.py
@@ -1,16 +1,18 @@
 import os
-import re
-import sys
 import yaml
 import time
 import argparse
+import logging
 import subprocess
-import update_hash
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--deployment", help="deployment yaml file to update", type=str,
  default=os.path.join(os.path.dirname(__file__), "../assisted-installer.yaml"))
 args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)-10s %(filename)s:%(lineno)d %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("__main__").setLevel(logging.INFO)
 
 def image_dose_not_exist(image, tag):
     cmd = f"docker manifest inspect quay.io/ocpmetal/{image}:{tag}"
@@ -21,16 +23,16 @@ def image_dose_not_exist(image, tag):
             image_inspect_output = "-1"
     return int(image_inspect_output)
 
-def image_dose_not_exist_with_retry(image, tag):
+def image_exist_with_retry(image, tag):
     for retry in range(3):
         get_image_result = image_dose_not_exist(image, tag)
         if not get_image_result:
-            return  get_image_result
-        print(f"Failed to get image at {retry} retry")
-        time.sleep(5*60)
+            return  True
+        logger.warning(f"Failed to get image at {retry} retry")
+        time.sleep(10*60)
 
-    print("Failed to get image information after 3 retries")
-    return get_image_result
+    logger.error(f"Failed to get image at {retry} retry")
+    return not get_image_result
 
 def main():
     with open(args.deployment, "r") as f:
@@ -38,11 +40,12 @@ def main():
     for rep, val in deployment.items():
         hash = val["revision"]
         for image in val["images"]:
-            if image_dose_not_exist_with_retry(image, hash):
-                print(f"image {image}:{hash} doesn't exist in registry")
-                raise Exception(f"image {image}:{hash} doesn't exist in registry")
+            if image_exist_with_retry(image, hash):
+                logger.info(f"image {image}:{hash} is located")
             else:
-                print(f"image {image}:{hash} is located")
+                logger.error(f"image {image}:{hash} doesn't exist in registry")
+                raise Exception(f"image {image}:{hash} doesn't exist in registry")
+
 
 
 if __name__ == "__main__":
