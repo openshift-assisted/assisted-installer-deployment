e52e2f20c9630fc096b6d1b3436aa6ec4800cffb
Riccardo piccoli <rpiccoli@redhat.com>
2022-03-08 12:06:19 +0100

    get vcs ref from latest image

--- tools/update_assisted_installer_yaml.py
+++ tools/update_assisted_installer_yaml.py
@@ -1,14 +1,42 @@
 import argparse
-import subprocess
 import yaml
 import os
 import update_hash
+import skopeo
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--deployment", help="deployment yaml file to update", type=str,
                     default=os.path.join(os.path.dirname(__file__), "../assisted-installer.yaml"))
 parser.add_argument('--full', help='update all hashes to master', action='store_true')
 args = parser.parse_args()
+
+
+def find_first_common_element(lists: list[str]) -> str:
+    if len(lists) < 1:
+        raise ValueError("No lists provided. Cannot find common element")
+
+    for el in lists[0]:
+        if all(el in list for list in lists[1:]):
+            return el
+
+    return None
+
+
+def get_ref_by_docker_image(images):
+    tag_pattern = "^latest-[a-f0-9]{40}$"
+    tag_prefix = "latest-"
+    skopeo_client = skopeo.Skopeo()
+    image_tags = [skopeo_client.get_image_tags_by_pattern(image, tag_pattern)
+                  for image in images]
+
+    if len(image_tags) == 0:
+        raise IndexError(f"Could not find any tags matching pattern `{tag_pattern}` for images `{images}`")
+
+    tag = find_first_common_element(image_tags)
+    if tag is None:
+        raise ValueError(f"Couild not find common tag for images {images}")
+
+    return tag.removeprefix(tag_prefix)
 
 
 def main():
@@ -19,13 +47,11 @@ def main():
 
     for rep in deployment:
         if full_release:
-            hash = subprocess.check_output(f"git ls-remote https://github.com/{rep}.git HEAD | cut -f 1", shell=True)[:-1]
-            hash = hash.decode("utf-8")
+            hash = get_ref_by_docker_image(deployment[rep]['images'])
         else:
             hash = os.environ.get(rep, None)
-            if not hash:
-                continue
-
+        if not hash:
+            continue
         update_hash.update_hash(deployment_yaml=args.deployment, repo=rep, hash=hash)
 
 
